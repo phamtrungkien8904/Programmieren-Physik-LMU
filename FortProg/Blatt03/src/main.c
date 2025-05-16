a4308599e28f69b3e13669e7a709947793b15067
Trung Kien Pham <Kien.Pham@campus.lmu.de>
2025-05-16 10:26:02 +0200

    update

--- FortProg/Blatt03/src/main.c
+++ FortProg/Blatt03/src/main.c
@@ -7,8 +7,8 @@
 // For now, it is enough to know that everytime the keyword after #define
 // shows up in the code, it will be replaced by what is directly behind it.
 #define DATA_SIZE 4096
-// #define ARRAY_SIZE (1<<24)
-#define ARRAY_SIZE 2048
+#define ARRAY_SIZE (1<<24)
+// #define ARRAY_SIZE 2048
 
 // Structs are custom data types. You can fill them with pretty much everything
 // you want. Think of them as the part of a class that holds all the variables
@@ -35,27 +35,28 @@ int main()
 {
     /////////////// My Sol ///////////////
     // Call the function and store the return value
-    data *lots_of_data = get_lots_of_data();
+    // data *lots_of_data = get_lots_of_data();
 
     ///////////// Fixing SegFault /////////////
     // Check if the memory allocation was successful
-    if (lots_of_data == NULL)
-    {
-        printf("Memory allocation failed\n");
-        return 1; // Exit the program with an error code
-    }
+    // if (lots_of_data == NULL)
+    // {
+    //     printf("Memory allocation failed\n");
+    //     return 1; // Exit the program with an error code
+    // }
+    ///////////// Fixing SegFault /////////////
     
     // Access the first int in the data structure
     // This might cause a segmentation fault if the memory allocation failed
-    int first_value = lots_of_data[0].a[0];
+    // int first_value = lots_of_data[0].a[0];
     
     // Alternatively using the -> operator
     // int first_value = lots_of_data->a[0];
     
-    printf("First value: %d\n", first_value);
+    // printf("First value: %d\n", first_value);
     
     // Free the allocated memory to prevent memory leaks
-    free(lots_of_data);
+    // free(lots_of_data);
     /////////////// My Sol /////////////////
 
     time_t start = get_clock();
